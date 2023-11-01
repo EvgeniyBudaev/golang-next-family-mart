53fb9469fff78dd333bf95f624941e029644cf89
Evgeniy Budaev <budaev.e@gmail.com>
2023-11-01 08:51:12 +0300

    fix: correctly path to auth

--- frontend/app/shared/api/auth/utils/loginApi.ts
+++ frontend/app/shared/api/auth/utils/loginApi.ts
@@ -3,7 +3,7 @@ import { TLogin, TLoginParams } from "@/app/shared/api/auth/types";
 import { EFormMethods } from "@/app/shared/form";
 
 export const loginApi: TApiFunction<TLoginParams, TLogin> = (params) => {
-  return fetchApi<TLogin>(`/api/v1/user/auth`, {
+  return fetchApi<TLogin>(`/api/v1/auth/login`, {
     method: EFormMethods.Post,
     body: params,
   });

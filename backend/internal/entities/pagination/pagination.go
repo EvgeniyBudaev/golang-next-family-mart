5b495c3e19e10af4b477d8cb7f8bc2635a508ced
Evgeniy Budaev <budaev.e@gmail.com>
2023-11-25 18:06:37 +0300

    feat: constPages

--- backend/internal/entities/pagination/pagination.go
+++ backend/internal/entities/pagination/pagination.go
@@ -11,6 +11,7 @@ import (
 type Pagination struct {
 	HasNext     bool   `json:"hasNext"`
 	HasPrevious bool   `json:"hasPrevious"`
+	CountPages  uint64 `json:"countPages"`
 	Limit       uint64 `json:"limit"`
 	Page        uint64 `json:"page"`
 	TotalItems  uint64 `json:"totalItems"`
@@ -20,6 +21,7 @@ func NewPagination(pag *Pagination) *Pagination {
 	return &Pagination{
 		HasNext:     pag.HasNext,
 		HasPrevious: pag.HasPrevious,
+		CountPages:  pag.CountPages,
 		Limit:       pag.Limit,
 		Page:        pag.Page,
 		TotalItems:  pag.TotalItems,
@@ -46,12 +48,18 @@ func GetTotalItems(ctx *fiber.Ctx, db *pgxpool.Pool, sqlBuilder sq.SelectBuilder
 	return totalItems, nil
 }
 
+func getCountPages(limit uint64, totalItems uint64) uint64 {
+	return (totalItems + limit - 1) / limit
+}
+
 func GetPagination(limit uint64, page uint64, totalItems uint64) *Pagination {
 	hasPrevious := page > 1
 	hasNext := (page * limit) < totalItems
+	constPages := getCountPages(limit, totalItems)
 	paging := NewPagination(&Pagination{
 		HasNext:     hasNext,
 		HasPrevious: hasPrevious,
+		CountPages:  constPages,
 		Limit:       limit,
 		Page:        page,
 		TotalItems:  totalItems,

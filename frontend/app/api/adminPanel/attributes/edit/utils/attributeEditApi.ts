9153c09e2d2bdc3589401e09da4a6dc34af9785e
Evgeniy Budaev <budaev.e@gmail.com>
2023-12-17 17:55:15 +0300

    remove log

--- frontend/app/api/adminPanel/attributes/edit/utils/attributeEditApi.ts
+++ frontend/app/api/adminPanel/attributes/edit/utils/attributeEditApi.ts
@@ -8,7 +8,6 @@ import { EFormMethods } from "@/app/shared/form";
 export const attributeEditApi: TApiFunction<TAttributeEditParams, TAttributeEditResponse> = (
   params,
 ) => {
-  console.log("api params: ", params);
   return fetchApi<TAttributeEditResponse>(`/api/v1/attribute/update`, {
     method: EFormMethods.Put,
     body: params,
